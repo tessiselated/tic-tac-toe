e0e988cc383a36e5dccf286f6b4e79efd82c0da4
Tess Elation <fredisded@gmail.com>
2016-11-29 19:55:49 +1100

    Added a function to check if there's a winner

--- scripts/main.js
+++ scripts/main.js
@@ -10,9 +10,10 @@ function createBoard() {
         var boardRow = document.createElement("div");
         boardRow.classList.add("row");
         for (j = 0; j < 3; j++) {
-          var boardBox = document.createElement("div");
-          boardBox.classList.add("col-md-4", "board-box", "empty");
-          boardRow.appendChild(boardBox);
+            var boardBox = document.createElement("div");
+            boardBox.classList.add("col-md-4", "board-box", "empty");
+            boardBox.id = "position" + i + j;
+            boardRow.appendChild(boardBox);
 
         }
         boardDOMElement.appendChild(boardRow);
@@ -29,12 +30,73 @@ var clickerCount = 0;
 
 boardDOMElement.addEventListener("click", function() {
     if (event.target.classList.contains("empty") && (clickerCount % 2 === 0)) {
-        event.target.classList.add("imperial");
+        event.target.classList.add("imperial", "in-play");
         event.target.classList.remove("empty");
         clickerCount++;
     } else if (event.target.classList.contains("empty") && (clickerCount % 2 != 0)) {
-        event.target.classList.add("alliance");
+        event.target.classList.add("alliance", "in-play");
         event.target.classList.remove("empty");
         clickerCount++;
     }
+    checkWinner();
+    console.log(gameWinner);
 })
+
+
+// After each click the game should check if someone wins
+// I need to write logic to check for this
+// I need a way to identify all elements in a row, column or diagonal
+
+
+// a row is position 00 position 01 and position 02
+// or position 10 position 11 and position 12
+// or position 20 position 21 and position 22
+
+var gameState = true;
+var gameWinner
+
+var winConditions = [
+    ["position00", "position01", "position02"],
+    ["position10", "position11", "position12"],
+    ["position20", "position21", "position22"],
+    ["position00", "position10", "position20"],
+    ["position01", "position11", "position21"],
+    ["position02", "position12", "position22"],
+    ["position00", "position11", "position22"],
+    ["position02", "position11", "position20"]
+];
+
+
+
+function checkWinner() {
+    var inPlay = document.getElementsByClassName("in-play"); //returns an object of all clicked elements
+    var allianceIds = [];
+    var imperialIds = [];
+    for (i = 0; i < inPlay.length; i++) {
+        if (inPlay[i].classList.contains("alliance")) {
+            allianceIds.push(inPlay[i].id);
+
+        } else if (inPlay[i].classList.contains("imperial")) {
+            imperialIds.push(inPlay[i].id);
+        }
+
+    }
+    for (j = 0; j < winConditions.length; j++) {
+        var didAllianceWin = winConditions[j].every(function(val) {
+            return allianceIds.indexOf(val) >= 0; });
+            if (didAllianceWin === true){
+                gameState = false;
+                gameWinner = "Alliance";
+            }
+    }
+    for (k = 0; k < winConditions.length; k++) {
+        var didImperialsWin = winConditions[k].every(function(val) {
+            return imperialIds.indexOf(val) >= 0; });
+            if (didImperialsWin === true) {
+                gameState = false;
+                gameWinner = "Imperials";
+            }
+
+    }
+
+}
